1868e122d15b8aef96cc1d2e0c8444e04290468f
Echo343 <Echo343@users.noreply.github.com>
2017-08-21 21:56:15 -0700

    Added complete recipe list for Stone Gem

--- src/main/java/com/blargsworkshop/sleepstone/ModItems.java
+++ src/main/java/com/blargsworkshop/sleepstone/ModItems.java
@@ -25,45 +25,64 @@ public class ModItems {
 	}
 	
 	public static void initRecipes() {
+		/** Sleepstone */
 		GameRegistry.addShapedRecipe(new ItemStack(itemSleepstone), "srs", "gbg", "srs",
 				's', new ItemStack(Blocks.stone),
 				'r', new ItemStack(Blocks.redstone_block),
 				'g', new ItemStack(Items.gold_ingot),
 				'b', new ItemStack(Items.bed));
 		
-		/**
-		 * item.stonecraftable_0.name=Obsidian Clump
-		 * item.stonecraftable_1.name=Hardened Obsidian Clump
-		 * item.stonecraftable_2.name=Lattice Diamond Encased Clump
-		 * item.stonecraftable_3.name=Hardened Crystallized Encased Structure
-		 * item.stonecraftable_4.name=Molded Ceramic Shield Ball
-		 * item.stonecraftable_5.name=Blasted Ceramic Encased Orb
-		 * item.stonecraftable_6.name=Laced Redstone Encased Orb
-		 * item.stonecraftable_7.name=Infused Redstone Orb
-		 * item.stonecraftable_8.name=Densely Packed Orb
-		 * 
-		 * Craftable from Obsidian, diamond blocks, sand, clay, redstone blocks & gold blocks.
+		/** Stone Gem
+		 * item.stonecraftable_0.name=Hardened Clay Piece
+		 * item.stonecraftable_1.name=Blasted Clay Piece
+		 * item.stonecraftable_2.name=Ceramic Foundation
+		 * item.stonecraftable_3.name=Blasted Ceramic Foundation
+		 * item.stonecraftable_4.name=Condensed Redstone Block
+		 * item.stonecraftable_5.name=Hyper Lattice Redstone Block
+		 * item.stonecraftable_6.name=Hyper Lattice Redstone Mass
+		 * item.stonecraftable_7.name=Hyper Infused Mass
+		 * item.stonecraftable_8.name=Refined Obsidian
+		 * item.stonecraftable_9.name=Refined Obsidian Clump
+		 * item.stonecraftable_10.name=Hardened Obsidian Clump
+		 * item.stonecraftable_11.name=Brittle Diamond Lattice
+		 * item.stonecraftable_12.name=Strengthened Diamond Lattice
+		 * item.stonecraftable_13.name=Crystalline Lattice Structure
+		 * item.stonecraftable_14.name=Heated Crystalline Lattice Structure
+		 * item.stonecraftable_15.name=Radial Empowered Orb
 		 * */
-		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 0), "xxx", "xxx", "xxx",
-				'x', new ItemStack(Blocks.obsidian));
-		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 0), new ItemStack(itemStoneCraftable, 1, 1), 1f);
-		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 2), "xxx", "xox", "xxx",
-				'x', Blocks.diamond_block,
-				'o', new ItemStack(itemStoneCraftable, 1, 1));
+		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 0), "ccc", "csc", "ccc",
+				'c', Blocks.hardened_clay,
+				's', Blocks.sandstone);
+		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 0), new ItemStack(itemStoneCraftable, 1, 1), 0.1f);
+		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 2), "xxx", "xcx", "xxx",
+				'x', new ItemStack(itemStoneCraftable, 1, 1),
+				'c', new ItemStack(Items.coal, 1, 1));
 		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 2), new ItemStack(itemStoneCraftable, 1, 3), 1f);
-		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 4), "csc", "sos", "csc",
-				'c', Blocks.clay,
-				's', Blocks.sand,
-				'o', new ItemStack(itemStoneCraftable, 1, 3));
-		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 4), new ItemStack(itemStoneCraftable, 1, 5), 1f);
-		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 6), "xxx", "xox", "xxx",
-				'x', Blocks.redstone_block,
-				'o', new ItemStack(itemStoneCraftable, 1, 5));
+		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 4), "xxx", "xxx", "xxx",
+				'x', Blocks.redstone_block);
+		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 5), "grg", "rgr", "grg",
+				'r', new ItemStack(itemStoneCraftable, 1, 4),
+				'g', Blocks.gold_block);
+		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 6), " r ", "rcr", " r ",
+				'r', new ItemStack(itemStoneCraftable, 1, 5),
+				'c', new ItemStack(itemStoneCraftable, 1, 3));
 		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 6), new ItemStack(itemStoneCraftable, 1, 7), 1f);
-		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 8), "xxx", "xox", "xxx",
-				'x', Blocks.gold_block,
-				'o', new ItemStack(itemStoneCraftable, 1, 7));
-		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 8), new ItemStack(itemStoneGem), 1f);
+		GameRegistry.addSmelting(new ItemStack(Blocks.obsidian), new ItemStack(itemStoneCraftable, 1, 8), 0.4f);
+		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 9), "ooo", "odo", "ooo",
+				'o', new ItemStack(itemStoneCraftable, 1, 8),
+				'd', Items.diamond);
+		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 9), new ItemStack(itemStoneCraftable, 1, 10), 0.7f);
+		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 11), "d d", " d ", "d d",
+				'd', Items.diamond);
+		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 11), new ItemStack(itemStoneCraftable, 1, 12), 1f);
+		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 13), "ooo", "odo", "ooo",
+				'o', new ItemStack(itemStoneCraftable, 1, 10),
+				'd', new ItemStack(itemStoneCraftable, 1, 12));
+		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 13), new ItemStack(itemStoneCraftable, 1, 14), 1f);
+		GameRegistry.addShapedRecipe(new ItemStack(itemStoneCraftable, 1, 15), "ccc", "cic", "ccc",
+				'c', new ItemStack(itemStoneCraftable, 1, 14),
+				'i', new ItemStack(itemStoneCraftable, 1, 7));
+		GameRegistry.addSmelting(new ItemStack(itemStoneCraftable, 1, 15), new ItemStack(itemStoneGem), 1f);		
 	}
 	
 	public static void initCreativeTabs() {
--- src/main/java/com/blargsworkshop/sleepstone/items/gems/support/StoneCraftable.java
+++ src/main/java/com/blargsworkshop/sleepstone/items/gems/support/StoneCraftable.java
@@ -12,22 +12,22 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 
 /**
- * item.densitycraftable_0.name=Hardened Clay Piece
- * item.densitycraftable_1.name=Blasted Clay Piece
- * item.densitycraftable_2.name=Ceramic Foundation
- * item.densitycraftable_3.name=Blasted Ceramic Foundation
- * item.densitycraftable_4.name=Condensed Redstone Block
- * item.densitycraftable_5.name=Hyper Lattice Redstone Block
- * item.densitycraftable_6.name=Hyper Lattice Redstone Mass
- * item.densitycraftable_7.name=Hyper Infused Mass
- * item.densitycraftable_8.name=Refined Obsidian
- * item.densitycraftable_9.name=Refined Obsidian Clump
- * item.densitycraftable_10.name=Hardened Obsidian Clump
- * item.densitycraftable_11.name=Brittle Diamond Lattice
- * item.densitycraftable_12.name=Strengthened Diamond Lattice
- * item.densitycraftable_13.name=Crystalline Lattice Structure
- * item.densitycraftable_14.name=Heated Crystalline Lattice Structure
- * item.densitycraftable_15.name=Radial Empowered Orb
+ * item.stonecraftable_0.name=Hardened Clay Piece
+ * item.stonecraftable_1.name=Blasted Clay Piece
+ * item.stonecraftable_2.name=Ceramic Foundation
+ * item.stonecraftable_3.name=Blasted Ceramic Foundation
+ * item.stonecraftable_4.name=Condensed Redstone Block
+ * item.stonecraftable_5.name=Hyper Lattice Redstone Block
+ * item.stonecraftable_6.name=Hyper Lattice Redstone Mass
+ * item.stonecraftable_7.name=Hyper Infused Mass
+ * item.stonecraftable_8.name=Refined Obsidian
+ * item.stonecraftable_9.name=Refined Obsidian Clump
+ * item.stonecraftable_10.name=Hardened Obsidian Clump
+ * item.stonecraftable_11.name=Brittle Diamond Lattice
+ * item.stonecraftable_12.name=Strengthened Diamond Lattice
+ * item.stonecraftable_13.name=Crystalline Lattice Structure
+ * item.stonecraftable_14.name=Heated Crystalline Lattice Structure
+ * item.stonecraftable_15.name=Radial Empowered Orb
  * 
  * @author Echo343
  *
@@ -46,7 +46,7 @@ public class StoneCraftable extends BaseItem {
 	@Override
 	public void registerIcons(IIconRegister reg) {
 		for (int i = 0; i < NUMBER_OF_CRAFTABLES; i++) {
-			this.icons[i] = reg.registerIcon(ModInfo.ID + ":stone-craftable_" + i);
+			this.icons[i] = reg.registerIcon(ModInfo.ID + ":stonecraftable_" + i);
 		}
 	}
 	
