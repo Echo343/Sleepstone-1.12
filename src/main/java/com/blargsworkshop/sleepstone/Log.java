c23e4d97c440d85b1f53e548e9e13b14b72d98f2
Echo343 <Echo343@users.noreply.github.com>
2017-10-13 21:39:26 -0700

    Added logger part 1

---
+++ src/main/java/com/blargsworkshop/sleepstone/Log.java
@@ -0,0 +1,85 @@
+package com.blargsworkshop.sleepstone;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+public class Log {
+	public static LogLevel Level = LogLevel.Detail;
+	public static boolean LogChat = true;
+	
+	public static enum LogLevel {
+		Off,
+		Error,
+		Info,
+		Debug,
+		Detail
+	};
+	
+	public static void error(String message) {
+		Log.error(message, null);
+	}
+	public static void error(String message, EntityPlayer player) {
+		Log.log(message, LogLevel.Error, player);
+	}
+	
+	public static void info(String message) {
+		Log.info(message, null);
+	}
+	public static void info(String message, EntityPlayer player) {
+		Log.log(message, LogLevel.Info, player);
+	}
+	
+	public static void debug(String message) {
+		Log.debug(message, null);
+	}
+	public static void debug(String message, EntityPlayer player) {
+		Log.log(message, LogLevel.Debug, player);
+	}
+	
+	public static void detail(String message) {
+		Log.detail(message, null);
+	}
+	public static void detail(String message, EntityPlayer player) {
+		Log.log(message, LogLevel.Detail, player);
+	}
+	
+	private static void log(String str, LogLevel level, EntityPlayer player) {
+		switch (Log.Level) {
+		case Off:
+			break;
+		case Error:
+			switch (level) {
+			case Error:
+				print(str, player);
+			default:
+				break;
+			}
+		case Info:
+			switch (level) {
+			case Error:
+			case Info:
+				print(str, player);
+			default:
+				break;
+			}
+		case Debug:
+			switch (level) {
+			case Error:
+			case Info:
+			case Debug:
+				print(str, player);
+			default:
+				break;
+			}
+		case Detail:
+			print(str, player);
+			break;		
+		}
+	}
+	
+	private static void print(String str, EntityPlayer player) {
+		System.out.println(str);
+		if (LogChat && player != null) {
+			Utils.addUnlocalizedChatMessage(player, str);
+		}
+	}
+}
--- src/main/java/com/blargsworkshop/sleepstone/ModInfo.java
+++ src/main/java/com/blargsworkshop/sleepstone/ModInfo.java
@@ -15,7 +15,6 @@ public class ModInfo {
 	public static final String VERSION = "1.2";
 	public static final String CLIENT_PROXY = "com.blargsworkshop.sleepstone.proxy.ClientProxy";
 	public static final String COMMON_PROXY = "com.blargsworkshop.sleepstone.proxy.CommonProxy";
-	public static final String CHANNEL = "";
 	
 	public static enum DEBUG {
 		NONE, CASUAL, DETAIL
