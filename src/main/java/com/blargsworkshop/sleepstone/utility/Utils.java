b940ea6a36f55211c7ee59d1e2587d11722559f0
Echo343 <Echo343@users.noreply.github.com>
2017-11-22 16:06:44 -0800

    Notes to fix Forge bug

--- src/main/java/com/blargsworkshop/sleepstone/utility/Utils.java
+++ src/main/java/com/blargsworkshop/sleepstone/utility/Utils.java
@@ -6,22 +6,25 @@ import java.util.Set;
 import com.blargsworkshop.sleepstone.items.gems.Gem;
 import com.blargsworkshop.sleepstone.items.stone.Slots;
 
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 
 public class Utils {
 
-	public static String localize(String messageKey) {
-//		return LanguageRegistry.instance().getStringLocalization(messageKey);
-		return messageKey;
+	public static String localize(String messageKey, Object... parameters) {
+		return I18n.format(messageKey, parameters);
 	}
 
 	public static void addChatMessage(EntityPlayer player, String messageKey) {
-//		player.addChatMessage(new ChatComponentText(localize(messageKey)));
+		// TODO Fix: from the server send custom commandMessage, on receiving client: localize and send as chat
+		player.sendMessage(new TextComponentString(isServer(player.getEntityWorld()) ? messageKey : localize(messageKey)));
 	}
 
 	public static void addUnlocalizedChatMessage(EntityPlayer player, String message) {
-//		player.addChatMessage(new ChatComponentText(message));
+		player.sendMessage(new TextComponentString(message));
 	}
 	
 	public static boolean isServer(World worldObj) {
@@ -78,4 +81,29 @@ public class Utils {
 		}
 		return gemTypeSet;
 	}
+	
+	/*
+Old GuiContainer
+
+if (iicon != null)
+{
+    GL11.glDisable(GL11.GL_LIGHTING);
+    GL11.glEnable(GL11.GL_BLEND); // Forge: Blending needs to be enabled for this.
+    this.mc.getTextureManager().bindTexture(TextureMap.locationItemsTexture);
+    this.drawTexturedModelRectFromIcon(i, j, iicon, 16, 16);
+    GL11.glDisable(GL11.GL_BLEND); // Forge: And clean that up
+    GL11.glEnable(GL11.GL_LIGHTING);
+    flag1 = true;
+}
+
+New GuiContainer line 280
+if (textureatlassprite != null)
+{
+    GlStateManager.disableLighting();
+    this.mc.getTextureManager().bindTexture(slotIn.getBackgroundLocation());
+    this.drawTexturedModalRect(i, j, textureatlassprite, 16, 16);
+    GlStateManager.enableLighting();
+    flag1 = true;
+}
+	 */
 }
