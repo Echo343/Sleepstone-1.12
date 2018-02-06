c908e35fcc9b1a85fe415a8312c2f21e83044a73
Echo343 <Echo343@users.noreply.github.com>
2018-02-06 10:59:40 -0800

    Fixed container shift+click for sleepstone gem inventory

--- src/main/java/com/blargsworkshop/sleepstone/items/stone/container/StoneContainer.java
+++ src/main/java/com/blargsworkshop/sleepstone/items/stone/container/StoneContainer.java
@@ -19,7 +19,11 @@ public class StoneContainer extends Container {
 	 * InventoryItem's number of slots (e.g. 5 slots is array indices 0-4, so start at 5)
 	 * Notice how we don't have to remember how many slots we made? We can just use
 	 * InventoryItem.INV_SIZE and if we ever change it, the Container updates automatically. */
-    private static final int INV_START = StoneInventory.INV_SIZE;
+    private static final int GEM_SLOTS_START = 0;
+    private static final int GEM_SLOTS_END = GemSlot.values().length - 1;
+    private static final int GEM_STORAGE_START = GEM_SLOTS_END + 1;
+    private static final int GEM_STORAGE_END = StoneInventory.INV_SIZE - 1;
+    private static final int INV_START = GEM_STORAGE_END + 1;
     private static final int INV_END = INV_START + 26;
     private static final int HOTBAR_START = INV_END + 1;
     private static final int HOTBAR_END = HOTBAR_START + 8;
@@ -99,94 +103,35 @@ public class StoneContainer extends Container {
     @Override
 	public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int index)
 	{
-		ItemStack itemstack = ItemStack.EMPTY;
+		ItemStack itemStackCopy = ItemStack.EMPTY;
 		Slot slot = this.inventorySlots.get(index);
 
 		if (slot != null && slot.getHasStack())
 		{
-			ItemStack itemstack1 = slot.getStack();
-			itemstack = itemstack1.copy();
+			ItemStack itemStack = slot.getStack();
+			itemStackCopy = itemStack.copy();
 
-			// If item is in our custom Inventory or armor slot
-			if (index < INV_START)
-			{
-				// try to place in player inventory / action bar
-				if (!this.mergeItemStack(itemstack1, INV_START, HOTBAR_END + 1, false))
-				{
+			if (index >= GEM_SLOTS_START && index <= GEM_SLOTS_END) {
+				if (!this.mergeItemStack(itemStack, GEM_STORAGE_START, HOTBAR_END + 1, false)) {
 					return ItemStack.EMPTY;
 				}
-
-//				slot.onSlotChange(itemstack1, itemstack);
 			}
-			// Item is in inventory / hotbar, try to place in custom inventory or armor slots
-			else
-			{
-				/*
-				If your inventory only stores certain instances of Items,
-				you can implement shift-clicking to your inventory like this:
-				
-				// Check that the item is the right type
-				if (itemstack1.getItem() instanceof ItemCustom)
-				{
-					// Try to merge into your custom inventory slots
-					// We use 'InventoryItem.INV_SIZE' instead of INV_START just in case
-					// you also add armor or other custom slots
-					if (!this.mergeItemStack(itemstack1, 0, InventoryItem.INV_SIZE, false))
-					{
-						return null;
-					}
-				}
-				// If you added armor slots, check them here as well:
-				// Item being shift-clicked is armor - try to put in armor slot
-				if (itemstack1.getItem() instanceof ItemArmor)
-				{
-					int type = ((ItemArmor) itemstack1.getItem()).armorType;
-					if (!this.mergeItemStack(itemstack1, ARMOR_START + type, ARMOR_START + type + 1, false))
-					{
-						return null;
-					}
-				}
-				Otherwise, you have basically 2 choices:
-				1. shift-clicking between player inventory and custom inventory
-				2. shift-clicking between action bar and inventory
-				 
-				Be sure to choose only ONE of the following implementations!!!
-				*/
-				/**
-				 * Implementation number 1: Shift-click into your custom inventory
-				 */
-				if (index >= INV_START)
-				{
-					// place in custom inventory
-					if (!this.mergeItemStack(itemstack1, 0, INV_START, false))
-					{
+			else if (index >= GEM_STORAGE_START && index <= GEM_STORAGE_END) {
+				if (!this.mergeItemStack(itemStack, GEM_SLOTS_START, GEM_SLOTS_END + 1, false)) {
+					if (!this.mergeItemStack(itemStack, INV_START, HOTBAR_END + 1, false)) {					
 						return ItemStack.EMPTY;
 					}
 				}
-				
-				/**
-				 * Implementation number 2: Shift-click items between action bar and inventory
-				 */
-				// item is in player's inventory, but not in action bar
-				// if (index >= INV_START && index < HOTBAR_START)
-				// {
-				// 	// place in action bar
-				// 	if (!this.mergeItemStack(itemstack1, HOTBAR_START, HOTBAR_END+1, false))
-				// 	{
-				// 		return null;
-				// 	}
-				// }
-				// // item in action bar - place in player inventory
-				// else if (index >= HOTBAR_START && index < HOTBAR_END+1)
-				// {
-				// 	if (!this.mergeItemStack(itemstack1, INV_START, INV_END+1, false))
-				// 	{
-				// 		return null;
-				// 	}
-				// }
 			}
-
-			if (itemstack1.isEmpty())
+			else {
+				if (!this.mergeItemStack(itemStack, GEM_SLOTS_START, GEM_SLOTS_END + 1, false)) {
+					if (!this.mergeItemStack(itemStack, GEM_STORAGE_START, GEM_STORAGE_END + 1, false)) {
+						return ItemStack.EMPTY;
+					}
+				}
+			}
+			
+			if (itemStack.isEmpty())
 			{
 				slot.putStack(ItemStack.EMPTY);
 			}
@@ -195,14 +140,12 @@ public class StoneContainer extends Container {
 				slot.onSlotChanged();
 			}
 
-			if (itemstack1.getCount() == itemstack.getCount())
+			if (itemStack.getCount() == itemStackCopy.getCount())
 			{
 				return ItemStack.EMPTY;
 			}
-
-//			slot.onTake(par1EntityPlayer, itemstack1);
 		}
-		return itemstack;
+		return itemStackCopy;
     }
     
     /**
