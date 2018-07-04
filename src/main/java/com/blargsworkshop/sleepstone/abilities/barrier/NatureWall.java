170c47d41891881aa83880c2950d36fd3653be71
Echo343 <Echo343@users.noreply.github.com>
2018-07-03 23:40:41 -0700

    Added vines to the leaf wall

--- src/main/java/com/blargsworkshop/sleepstone/abilities/barrier/NatureWall.java
+++ src/main/java/com/blargsworkshop/sleepstone/abilities/barrier/NatureWall.java
@@ -1,10 +1,14 @@
 package com.blargsworkshop.sleepstone.abilities.barrier;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -16,13 +20,52 @@ public class NatureWall {
 		List<BlockPos> shape = getShape();
 		
 		for (int y = -1; y <= 2; y++) {
+			// TODO make a different kind of leaves.
 			for (BlockPos shapeFragment : shape) {
 				BlockPos p = playerPosition.add(shapeFragment.getX(), y, shapeFragment.getZ());
 				if (world.getBlockState(p).getBlock().isReplaceable(world, p)) {
 					world.setBlockState(p, Blocks.LEAVES.getDefaultState());
 				}
 			}
+			for ( Entry<EnumFacing, List<BlockPos>> direction : getVinesMap().entrySet()) {
+				for (BlockPos vine : direction.getValue()) {
+					BlockPos p = playerPosition.add(vine.getX(), y, vine.getZ());
+					if (world.getBlockState(p).getBlock().isReplaceable(world, p)) {
+						world.setBlockState(p, Blocks.VINE.getStateForPlacement(null, p, direction.getKey(), 0, 0, 0, 0, null, null));
+					}
+				}
+			}
 		}
+	}
+	
+	protected Map<EnumFacing, List<BlockPos>> getVinesMap() {
+		// TODO make enumMap
+		Map<EnumFacing, List<BlockPos>> vines = new HashMap<>();
+		List<BlockPos> north = new ArrayList<>();
+		north.add(new BlockPos(-1, 0, 2));
+		north.add(new BlockPos(0, 0, 2));
+		north.add(new BlockPos(1, 0, 2));
+		vines.put(EnumFacing.NORTH, north);
+		
+		List<BlockPos> east = new ArrayList<>();
+		east.add(new BlockPos(-2, 0, 1));
+		east.add(new BlockPos(-2, 0, 0));
+		east.add(new BlockPos(-2, 0, -1));
+		vines.put(EnumFacing.EAST, east);
+		
+		List<BlockPos> south = new ArrayList<>();
+		south.add(new BlockPos(-1, 0, -2));
+		south.add(new BlockPos(0, 0, -2));
+		south.add(new BlockPos(1, 0, -2));
+		vines.put(EnumFacing.SOUTH, south);
+		
+		List<BlockPos> west = new ArrayList<>();
+		west.add(new BlockPos(2, 0, 1));
+		west.add(new BlockPos(2, 0, 0));
+		west.add(new BlockPos(2, 0, -1));
+		vines.put(EnumFacing.WEST, west);
+		
+		return vines;
 	}
 	
 	protected List<BlockPos> getShape() {
