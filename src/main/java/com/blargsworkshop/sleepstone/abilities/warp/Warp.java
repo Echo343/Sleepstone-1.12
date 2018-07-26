b85e3631f9d4e5d4747ccc85cdc5848eaf0f80b5
Echo343 <Echo343@users.noreply.github.com>
2018-07-25 17:25:40 -0700

    Made Warp check before and after channel.

--- src/main/java/com/blargsworkshop/sleepstone/abilities/warp/Warp.java
+++ src/main/java/com/blargsworkshop/sleepstone/abilities/warp/Warp.java
@@ -1,5 +1,7 @@
 package com.blargsworkshop.sleepstone.abilities.warp;
 
+import javax.annotation.Nonnull;
+
 import com.blargsworkshop.engine.logger.Log;
 import com.blargsworkshop.engine.sound.SoundManager;
 import com.blargsworkshop.engine.utility.SimpleTeleporter;
@@ -16,18 +18,15 @@ import net.minecraft.world.World;
 public enum Warp {
 	INSTANCE;
 	
-	public void warpPlayerToBed(EntityPlayerMP player) {
+	private BlockPos getValidWarpLocation(@Nonnull EntityPlayerMP player) {
+		BlockPos validWarpLocation = null;
 		World world = player.getEntityWorld();
 		if (Utils.isServer(world)) {
 			BlockPos bedPos = player.getBedLocation();
 			if (bedPos != null) {
 				BlockPos bedSpawnPos = EntityPlayer.getBedSpawnLocation(world, bedPos, false);
 				if (bedSpawnPos != null) {
-					SoundManager.playSoundAtEntityFromServer(player, Sounds.swoosh);
-					SimpleTeleporter.INSTANCE.teleportPlayerWithinDimension(player, bedSpawnPos, true);
-					player.addPotionEffect(new WarpSicknessPotionEffect());
-					SoundManager.playSoundAtEntityFromServer(player, Sounds.teleport);
-					Log.debug("Warping to: " + (bedSpawnPos.getX()) + ", " + (bedSpawnPos.getY()) + ", " + (bedSpawnPos.getZ()), player);
+					validWarpLocation = bedSpawnPos;
 				}
 				else {
 					Utils.addStatusMessage(player, "text.sleepstone.bed_destroyed");
@@ -37,14 +36,26 @@ public enum Warp {
 				Utils.addStatusMessage(player, "text.sleepstone.no_spawn_point");
 			}
 		}
+		return validWarpLocation;
 	}
 	
 	public void startWarp(EntityPlayerMP player) {
 		if (player.isPotionActive(Potions.warpSickness)) {
 			Utils.addStatusMessage(player, "text.sleepstone.suffering_effects_of_warping");
 		}
-		else {
+		else if (getValidWarpLocation(player) != null) {
 			player.addPotionEffect(new WarpPotionEffect(player));
+		}
+	}
+	
+	public void warpPlayerToBed(EntityPlayerMP player) {
+		BlockPos bedSpawnPos = getValidWarpLocation(player);
+		if (bedSpawnPos != null) {
+			SoundManager.playSoundAtEntityFromServer(player, Sounds.swoosh);
+			SimpleTeleporter.INSTANCE.teleportPlayerWithinDimension(player, bedSpawnPos, true);
+			player.addPotionEffect(new WarpSicknessPotionEffect());
+			SoundManager.playSoundAtEntityFromServer(player, Sounds.teleport);
+			Log.debug("Warping to: " + (bedSpawnPos.getX()) + ", " + (bedSpawnPos.getY()) + ", " + (bedSpawnPos.getZ()), player);
 		}
 	}
 
